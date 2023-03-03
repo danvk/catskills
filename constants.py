04d9fa3cbcb9d4d4068c07a9113b57b398e07d35
Dan Vanderkam <danvdk@gmail.com>
2023-03-03 16:28:07 -0500

    generate hiking log JSON

---
+++ constants.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+BLOG_ROOT = Path(__file__).parent.parent / 'danvk.github.io' / 'catskills'
--- make_hikes_json.py
+++ make_hikes_json.py
@@ -1,1 +1,51 @@
 #!/usr/bin/env python
+"""Read YAML front matter from posts to make combined hikes.json file."""
+
+import json
+import yaml
+import sys
+
+from constants import BLOG_ROOT
+
+
+def read_frontmatter(stream) -> dict:
+    lines = []
+    on = False
+    for line in stream:
+        if line == '---\n':
+            if on:
+                break
+            else:
+                on = True
+                continue
+        if on:
+            lines.append(line)
+    frontmatter = '\n'.join(lines)
+    return yaml.safe_load(frontmatter)
+
+
+def format_meta(meta: dict) -> dict:
+    return {
+        'title': meta['title'],
+        'date': meta['Date'].strftime('%Y-%m-%d'),
+        'type': meta['Type'],
+        'slug': meta['slug'],
+        'miles': meta['Mileage (mi)'],
+        'hike_hours': meta['Hiking Time (h)'],
+        'peaks': meta['Peaks'].split(', '),
+        'hikers': ['DanVK'] + meta['Other Participants'].split(', ')
+    }
+
+
+def main():
+    hikes = []
+    for path in BLOG_ROOT.glob('20*/*.md'):
+        sys.stderr.write(str(path) + '\n')
+        meta = read_frontmatter(open(path))
+        hikes.append(format_meta(meta))
+    hikes.sort(key=lambda hike: hike['date'])
+    json.dump(hikes, sys.stdout, indent=2)
+
+
+if __name__ == '__main__':
+    main()

c2de9dcf4d11a943126f442c56206cc628a2f1d5
Dan Vanderkam <danvdk@gmail.com>
2023-03-24 12:55:01 -0400

    Display Catskill park

--- map-vite/src/HikeMap.tsx
+++ map-vite/src/HikeMap.tsx
@@ -1,4 +1,4 @@
-import Map, { Marker } from "react-map-gl";
+import Map, { Layer, Marker, Source } from "react-map-gl";
 
 import { UseQueryResult } from "@tanstack/react-query";
 import { Hike } from "./HikeList";
@@ -12,19 +12,37 @@ export interface Props {
   hikes: UseQueryResult<readonly Hike[], unknown>;
 }
 
+export const parkStyle = {
+  type: "line",
+  layout: {
+    "line-join": "round",
+    "line-cap": "round",
+  },
+  paint: {
+    "line-color": "brown",
+    "line-width": 1,
+  },
+} satisfies Partial<mapboxgl.AnyLayer>;
+
 export function HikeMap(props: Props) {
   return (
     <div id="map">
       <Map
         initialViewState={{
-          latitude: 37.8,
-          longitude: -122.4,
-          zoom: 14,
+          latitude: 42.0922169187148,
+          longitude: -74.36398700976565,
+          zoom: 10,
         }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
+        mapStyle="mapbox://styles/danvk/clf7a8rz5001j01qerupylm4t"
         mapboxAccessToken={MAPBOX_TOKEN}
       >
-        <Marker longitude={-122.4} latitude={37.8} color="red" />
+        <Source
+          id="catskill-park"
+          type="geojson"
+          data="catskill-park.geojson"
+        >
+          <Layer id="catskill-park" {...parkStyle} />
+        </Source>
       </Map>
     </div>
   );

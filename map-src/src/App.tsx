4022846f2e2efec2c8dce33da03b0d4afdc35682
Dan Vanderkam <danvdk@gmail.com>
2023-03-25 11:46:07 -0400

    vite-based production map

--- map-src/src/App.tsx
+++ map-src/src/App.tsx
@@ -11,7 +11,7 @@ import React from "react";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HikePage />}></Route>
+      <Route path="/catskills/map" element={<HikePage />}></Route>
     </Routes>
   );
 }

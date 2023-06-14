8c98eee277c965788701f0f8556ba4b9f2d50e94
Dan Vanderkam <danvdk@gmail.com>
2023-06-14 10:11:25 -0400

    bespoke URL parameters

--- map-src/src/HikePlanner.tsx
+++ map-src/src/HikePlanner.tsx
@@ -4,7 +4,7 @@ import {Feature, FeatureCollection} from 'geojson';
 import _ from 'lodash';
 import React from 'react';
 import Map, {Layer, Source} from 'react-map-gl';
-import {useSearchParams} from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import {EMPTY_FC, MAPBOX_TOKEN, MountainPeaks, parkStyle} from './HikeMap';
 
@@ -95,8 +95,47 @@ export interface SuccessState<T> {
 /** A deferred/promised value can be in one of three states: loading, error or success. */
 export type PromiseState<T> = LoadingState | ErrorState | SuccessState<T>;
 
+export function lightlyEncodeURIComponent(param: string) {
+  return param.replace(/\+/g, '%2B').replace(/ /g, '+').replace(/&/g, '%26');
+}
+
+export function lightlyDecodeURIComponent(param: string) {
+  return param.replace(/%26/g, '&').replace(/\+/g, ' ').replace(/%2B/g, '+');
+}
+
+function createSearchParams(params?: Record<string, string>): string {
+  return Object.entries(params ?? {}).map(([k, v]) => k + '=' + lightlyEncodeURIComponent(v)).join('&');
+}
+
+export type SetURLSearchParams = (
+  nextInit?:
+    | Record<string, string>
+    | ((prev: URLSearchParams) => Record<string, string>),
+) => void;
+
+// See https://github.com/remix-run/react-router/blob/fe661c5c0405c2212a5299b75af362df9f031b11/packages/react-router-dom/index.tsx#L904
+function useLightlyEncodedSearchParams(): [URLSearchParams, SetURLSearchParams] {
+  const location = useLocation();
+  const searchParams = React.useMemo(
+    () => new URLSearchParams(location.search),
+    [location.search],
+  );
+  const navigate = useNavigate();
+  const setSearchParams = React.useCallback<SetURLSearchParams>(
+    (nextInit) => {
+      const newSearchParams = createSearchParams(
+        typeof nextInit === 'function' ? nextInit(searchParams) : nextInit,
+      );
+      navigate('?' + newSearchParams, {replace: true});
+    },
+    [navigate, searchParams],
+  );
+
+  return [searchParams, setSearchParams];
+}
+
 export function HikePlanner() {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useLightlyEncodedSearchParams();
   const peaksParam = searchParams.get('peaks');
   const peaks = (peaksParam === null ? ALL_PEAKS : peaksParam.split(',')) as Peak[];
   const mode = (searchParams.get('mode') ?? 'unrestricted') as Mode;
